1ed63447c2d4024795054b5fab1c8ea905f8c510
Jan Solanti <jhs@psonet.com>
2019-10-04 00:15:46 +0300

    Update copyright years

--- squish_cli/src/main.rs
+++ squish_cli/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Jan Solanti <jhs@psonet.com>
+// Copyright (c) 2018-2019 Jan Solanti <jhs@psonet.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -208,7 +208,7 @@ fn parallel_compress(format: Format, image: &RawImage, buf: &mut [u8], params: P
                     // get position in source image
                     let sx = 4 * x + px;
                     let sy = 4 * y + py;
-                    
+
                     // enable pixel if within bounds
                     if sx < image.width && sy < image.height {
                         // copy pixel value
