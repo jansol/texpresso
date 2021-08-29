cc61c5417f33c9460d65855f90910213aab3a58d
Jan Solanti <jhs@psonet.com>
2021-08-29 16:28:29 +0300

    Update copyright years

--- squish/src/lib.rs
+++ squish/src/lib.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2006 Simon Brown <si@sjbrown.co.uk>
-// Copyright (c) 2018-2019 Jan Solanti <jhs@psonet.com>
+// Copyright (c) 2018-2021 Jan Solanti <jhs@psonet.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
